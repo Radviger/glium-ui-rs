b8d39d87e72aa2ef041b8de4c26eb3fe470b35dc
Radviger <radviger@gmail.com>
2020-02-12 20:07:34 +0200

    Fixed button text align.

--- src/ui.rs
+++ src/ui.rs
@@ -303,9 +303,9 @@ impl<S> Widget<S> for Button where S: Surface {
         let bounds = [x, y, w, h];
         let background = if self.hover { &self.background_hover } else { &self.background_normal };
         background.draw(canvas, bounds, partial_ticks);
-        canvas.text(&self.label, x + w / 2.0, y + 4.0, &FontParameters {
+        canvas.text(&self.label, x + w / 2.0, y + h / 2.0, &FontParameters {
             color: [1.0; 4],
-            align_horizontal: TextAlignHorizontal::Left,
+            align_horizontal: TextAlignHorizontal::Center,
             align_vertical: TextAlignVertical::Center,
             .. Default::default()
         });
